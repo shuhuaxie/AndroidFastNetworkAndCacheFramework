c887148d6e9cb919e0ab7df95fc3162d9d2f4542
xieshuhua <xieshuhua@163.com>
2015-12-30 10:41:13 +0800

    Add libs needed and simplify the code

--- app/src/main/java/android/frame/fastnetworkandcacheframe/MainActivity.java
+++ app/src/main/java/android/frame/fastnetworkandcacheframe/MainActivity.java
@@ -1,15 +1,9 @@
 package android.frame.fastnetworkandcacheframe;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
